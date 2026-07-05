495e257f4fb89ae9ae19a7c63d25852b2a4ddfc8
Udayan5757 <udayanbiswas577@gmail.com>
2026-07-05 13:42:29 +0530

    small change

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-
 from src.api.routes import router
 from src.api.upload_routes import router as upload_router
-from src.api.rebuild_routes import router as rebuild_router
 
 app = FastAPI(
     title="Enterprise AI Knowledge Assistant",
@@ -11,4 +9,3 @@ app = FastAPI(
 
 app.include_router(router)
 app.include_router(upload_router)
-app.include_router(rebuild_router)
